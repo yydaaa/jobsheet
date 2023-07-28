d6678a17cc89a7dcf799e2fee424cb26a036a865
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-07-28 17:31:36 +0700

    game tebak angka

---
+++ guess.py
@@ -0,0 +1,10 @@
+class guess:
+    def __init__(self):
+        self.t=""
+        self.j=""
+
+def cek(self):
+    if self.t == self.j:
+        return true
+    return false
+    
---
+++ jobsheet1.py
@@ -0,0 +1,9 @@
+import random
+from guess import guess
+
+jawaban = random.randint1(1,10)
+t=int(input('tebak angka 1 s.d 10:'))
+if ():
+    print("jawaban kamu benar!")
+else:
+    print("jawaban kamu salah!")
