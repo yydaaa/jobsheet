ead4072d8fc31dcd83a963d6ab61b2c1d5157889
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-08-22 13:44:34 +0700

    jobsheet1.py

--- jobsheet1.py
+++ jobsheet1.py
@@ -1,7 +1,7 @@
 import random
 from guess import guess
 
-jawaban = random.randint1(1,10)
+jawaban = random.randint(1,10)
 t=int(input('tebak angka 1 s.d 10:'))
 if ():
     print("jawaban kamu benar!")
